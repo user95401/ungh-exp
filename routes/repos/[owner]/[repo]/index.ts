409ccf05f0519693b42a5018b1eba6b893400146
user95401 <90561697+user95401@users.noreply.github.com>
2025-07-05 13:34:44 +0000

    huh?

--- routes/repos/[owner]/[repo]/index.ts
+++ routes/repos/[owner]/[repo]/index.ts
@@ -1,24 +1,7 @@
 import type { GithubRepo } from "~types";
 
 export default eventHandler(async (event) => {
-  const rawRepo = await ghRepo(
+  return ghRepo(
     `${event.context.params.owner}/${event.context.params.repo}`,
   );
-  const repo = <GithubRepo>{
-    id: rawRepo.id,
-    name: rawRepo.name,
-    repo: rawRepo.full_name,
-    description: rawRepo.description,
-    createdAt: rawRepo.created_at,
-    updatedAt: rawRepo.updated_at,
-    pushedAt: rawRepo.pushed_at,
-    stars: rawRepo.stargazers_count,
-    watchers: rawRepo.subscribers_count,
-    forks: rawRepo.forks,
-    defaultBranch: rawRepo.default_branch,
-  };
-
-  return {
-    repo,
-  };
 });
