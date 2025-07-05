c0f1a9976016b38e3ba71a9e58bb4c8741e2fe30
user95401 <90561697+user95401@users.noreply.github.com>
2025-07-05 13:51:12 +0000

    repos

--- routes/repos/[owner]/[repo]/releases/index.ts
+++ routes/repos/[owner]/[repo]/releases/index.ts
@@ -3,45 +3,5 @@ import type { GithubRelease } from "~types";
 export default eventHandler(async (event) => {
   const repo = `${event.context.params.owner}/${event.context.params.repo}`;
   const res = await ghFetch(`/repos/${repo}/releases`);
-
-  const releases = res.map(
-    (i) =>
-      <GithubRelease>{
-        id: i.id,
-        tag: i.tag_name,
-        author: i.author.login,
-        name: i.name,
-        draft: i.draft,
-        prerelease: i.prerelease,
-        createdAt: i.created_at,
-        publishedAt: i.published_at,
-        markdown: i.body,
-        html: "",
-        assets:
-          "assets" in i
-            ? i.assets.map((a) => ({
-                contentType: a.content_type,
-                size: a.size,
-                createdAt: a.created_at,
-                updatedAt: a.updated_at,
-                downloadCount: a.download_count,
-                downloadUrl: a.browser_download_url,
-              }))
-            : [],
-      },
-  );
-
-  await Promise.all(
-    releases.map(async (release) => {
-      release.html = await ghMarkdown(
-        release.markdown,
-        repo,
-        "release-" + release.tag,
-      );
-    }),
-  );
-
-  return {
-    releases,
-  };
+  return res;
 });
--- routes/repos/[owner]/[repo]/releases/latest.ts
+++ routes/repos/[owner]/[repo]/releases/latest.ts
@@ -3,32 +3,5 @@ import type { GithubRelease } from "~types";
 export default eventHandler(async (event) => {
   const repo = `${event.context.params.owner}/${event.context.params.repo}`;
   const i = await ghFetch(`repos/${repo}/releases/latest`);
-
-  const release: GithubRelease = {
-    id: i.id,
-    tag: i.tag_name,
-    author: i.author.login,
-    name: i.name,
-    draft: i.draft,
-    prerelease: i.prerelease,
-    createdAt: i.created_at,
-    publishedAt: i.published_at,
-    markdown: i.body,
-    html: await ghMarkdown(i.body, repo, "release-" + i.tag),
-    assets:
-      "assets" in i
-        ? i.assets.map((a) => ({
-            contentType: a.content_type,
-            size: a.size,
-            createdAt: a.created_at,
-            updatedAt: a.updated_at,
-            downloadCount: a.download_count,
-            downloadUrl: a.browser_download_url,
-          }))
-        : [],
-  };
-
-  return {
-    release,
-  };
+  return i;
 });
